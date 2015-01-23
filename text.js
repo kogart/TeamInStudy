b5a3f578b76d274b5e26945bd735006788967f75
MacketSWE <mthorelius@gmail.com>
2015-01-23 09:51:50 +0100

    Sida - test
    La upp en ny sida med css, html och js

--- text.js
+++ text.js
@@ -2,11 +2,11 @@ var InStudy = "Hej allihopa!";
 var Team = "Hej!";
 var Klocka = 1135;
 
-console.log("program start")
-alert("Tjoho!")
+console.log("program start");
+alert("Tjoho!");
 
 
 
 
-console.log("program end")
-alert("Tjoho!")
+console.log("program end");
+alert("Tjoho!");
